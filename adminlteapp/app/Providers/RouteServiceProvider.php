aae100e2dd242c50c6078154ebad977305dc5fbf
okabaa <Alanya-07>
2021-01-10 00:45:03 +0300

    AdminLTE Örnek Sayfa AdminLTE vendor klsörü taşındı Ayrı Route dosyası Route dosyasına provider üzerinde ayarlamalar  -Auth kontrolü,  -prefix  -namespace Örnek controller ve klsör Örnek view ve klasör

--- adminlteapp/app/Providers/RouteServiceProvider.php
+++ adminlteapp/app/Providers/RouteServiceProvider.php
@@ -46,6 +46,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware(['web', 'auth:sanctum', 'verified'])
+                ->prefix('admin')
+                ->namespace('App\Http\Controllers\Admin')
+                ->group(base_path('routes/admin.php'));
         });
     }
 
